07d0d3d541da8af90c29cdbce4222fdc9ff0000d
dean <64692815+deant-digital@users.noreply.github.com>
2026-01-10 12:00:00 +0800

    feat: add 2021 Preliminary Q10

---
+++ 2021/preliminary/q10.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  double t;
+  cin >> t;
+  cout << fixed << setprecision(2) << t / (1 / 22.0 + 1 / 30.0) << '\n';
+}
